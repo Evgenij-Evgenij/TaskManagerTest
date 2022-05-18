2cb3ee68bc883b57604fb0ec4e7e8029b95b91a8
Evgenij <evgenijvoronov150@gmail.com>
2022-05-18 11:30:06 +0300

    Initial Commit

---
+++ DesignScreen/DesignScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignScreenApp.swift
+//  DesignScreen
+//
+//  Created by Евгений Воронов on 18.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
